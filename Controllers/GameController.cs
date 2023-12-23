817d76c7d01664c848e9c4f08ea7629e99d4889a
Gabrielle Lattanzi <g.lattanzi26@gmail.com>
2023-12-23 19:43:50 -0300

    Creating Game controller

---
+++ Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamesAPI.Controllers
+{
+    [Route("api/games")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public IActionResult Test()
+        {
+            return Ok("Working...");
+        }
+    }
+}
